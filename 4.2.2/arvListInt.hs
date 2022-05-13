3e3e7ea52745a2556843fd8861afd64910a8e0b2
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2022-05-13 15:13:20 -0300

    att

--- 4.2.2/arvListInt.hs
+++ 4.2.2/arvListInt.hs
@@ -10,13 +10,11 @@ arv =
             (No Folha [23] Folha)
         )
         [4,5,6]
-        ( No Folha [13,14] Folha
-        )
+        ( No Folha [13,14] Folha)
     )
     [1,2,3]
     ( No
-        ( No Folha [15,16,17] Folha
-        )
+        ( No Folha [15,16,17] Folha)
         [7,8,9,10]
         ( No
             (No Folha [24] Folha)
