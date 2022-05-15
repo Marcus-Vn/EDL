a0d745f5c6b89006244fd6fd4bb5d285b30e5f75
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2022-05-15 11:17:14 -0300

    QuickSort para o exemplo

---
+++ 3.1.2/quicksort.py
@@ -0,0 +1,35 @@
+def particao(array, inicio, fim):
+    pivo = array[inicio]
+    low = inicio + 1
+    high = fim
+
+    while True:
+
+        while low <= high and array[high] >= pivo:
+            high = high - 1
+
+        while low <= high and array[low] <= pivo:
+            low = low + 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[inicio], array[high] = array[high], array[inicio]
+
+    return high
+
+def quick_sort(array, inicio, fim):
+    if inicio >= fim:
+        return
+
+    p = particao(array, inicio, fim)
+    quick_sort(array, inicio, p-1)
+    quick_sort(array, p+1, fim)
+
+
+array = [9,2,3,4,11,13,8,5,1,14,6,10,15,7,12]
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
